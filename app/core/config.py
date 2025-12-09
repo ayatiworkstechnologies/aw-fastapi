9a06b830d655c6cc79ed45180017690390a9c350
rubankumarsankar <rubankumarsankar@users.noreply.github.com>
2025-12-09 18:49:43 +0530

    09/12/2025 added structerd backend 5

--- app/core/config.py
+++ app/core/config.py
@@ -2,11 +2,10 @@ import os
 
 class Settings:
     DATABASE_URL: str = os.getenv(
-        
-        DATABASE_URL = "mysql+pymysql://awadmin:ayati123@localhost:3306/aw_admin?charset=utf8mb4"
-
-        # "mysql+pymysql://root:ayati@localhost:3306/aw_admin?charset=utf8mb4",
+        "DATABASE_URL",
+        "mysql+pymysql://awadmin:ayati123@localhost:3306/aw_admin?charset=utf8mb4"
     )
+
     SECRET_KEY: str = os.getenv("SECRET_KEY", "dev-secret-key")
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_HOURS: int = 12
