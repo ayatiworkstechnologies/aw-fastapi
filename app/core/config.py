e90f80c58d7aed10864abf394f752781d714ce62
rubankumarsankar <rubankumarsankar@users.noreply.github.com>
2025-12-17 12:51:55 +0530

    17/12/2025 update 1

--- app/core/config.py
+++ app/core/config.py
@@ -3,8 +3,8 @@ import os
 class Settings:
     DATABASE_URL: str = os.getenv(
         "DATABASE_URL",
-        # "mysql+pymysql://awadmin:ayati123@localhost:3306/aw_admin?charset=utf8mb4"
-        "mysql+pymysql://root:ayati@localhost:3306/aw_admin?charset=utf8mb4"
+        "mysql+pymysql://awadmin:ayati123@localhost:3306/aw_admin?charset=utf8mb4"
+        # "mysql+pymysql://root:ayati@localhost:3306/aw_admin?charset=utf8mb4"
     )
 
     SECRET_KEY: str = os.getenv("SECRET_KEY", "dev-secret-key")
